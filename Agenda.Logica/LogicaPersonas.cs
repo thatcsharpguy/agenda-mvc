65b430922b1efb23aea60ababe1106e5b36e93c0
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-25 15:15:41 -0500

    Crear logica personas

---
+++ Agenda.Logica/LogicaPersonas.cs
@@ -0,0 +1,30 @@
+﻿using Agenda.Datos;
+using Agenda.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Logica
+{
+    public class LogicaPersonas
+    {
+        private RepositorioPersonas _repositorioPersonas;
+
+        public LogicaPersonas()
+        {
+            _repositorioPersonas = new RepositorioPersonas();
+        }
+
+        public void InsertaPersona(Persona persona)
+        {
+            _repositorioPersonas.InsertaPersona(persona);
+        }
+
+        public Persona ObtienePersona(int id)
+        {
+            return _repositorioPersonas.ObtienePersona(id);
+        }
+    }
+}
