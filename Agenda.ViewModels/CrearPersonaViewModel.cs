77cc4d67acf9cac1c2f2d1cddea0b9625270a55a
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-25 15:19:14 -0500

    Crear ViewModels

---
+++ Agenda.ViewModels/CrearPersonaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.ViewModels
+{
+    public class CrearPersonaViewModel
+    {
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+    }
+}
